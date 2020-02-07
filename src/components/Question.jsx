74b14f04aeca5c483da908c4a9f6b3fbc78b9588
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-07 17:21:49 -0500

    Feat - hanlde Form input values to state and submition

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react'
 
 const Question = () => {
+
+    const [ quantity, setQuantity ] = useState(0)
+
+    const [ error, setError] = useState(false)
+
+    const setBudget = e => {
+        setQuantity(parseInt(e.target.value))
+    }
+
+    const addBudget = e => {
+        e.preventDefault();
+
+        if(quantity < 1 || isNaN(quantity)) {
+            setError(true)
+            return
+        }
+
+        setError(false)
+    }
+
     return ( 
         <Fragment>
             <h2>Type your budget</h2>
-            <form>
+            
+            <form onSubmit={addBudget}>
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Type your budget"
+                    onChange={setBudget}
+                    value={quantity}
                 />
                 <button
                     type="submit"
@@ -21,4 +44,4 @@ const Question = () => {
     );
 }
  
-export default Question;
+export default Question
