f92abe719d3131c3e700cbd94fe4087b328d54b0
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-07 18:41:41 -0500

    Feat - add form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
+import Error from './Error'
 
 const Form = () => {
 
     const [name, setName] = useState('');
     const [quantity, setQuantity] = useState(0);
+    const [error, setError] = useState(false)
+
     const addExpense = e => {
         e.preventDefault()
+        // validate
+        if(quantity<1 || isNaN(quantity) || name.trim() === '') {
+            setError(true)
+            return
+        }
+        setError(false)
     }
     return ( 
         <form onSubmit={addExpense}>
             <h2>Add your expenses here</h2>
-
+            { error ? <Error message="both fields are required o budget wrong!"/> : null}
             <div className="field">
                 <label>Expense name</label>
                 <input 
